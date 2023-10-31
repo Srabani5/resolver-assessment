d2c601256e5fe3c923f49ab3304274b9f1b54f90
Srabani "Bonnie" Banik <srabani.banik5@gmail.com>
2023-10-30 23:51:21 -0400

    WIP: Create utils package for capturing screenshots

---
+++ src/test/java/com/resolver/utils/ScreenshotUtils.java
@@ -0,0 +1,29 @@
+package com.resolver.utils;
+
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+
+/**
+ * A utility class for capturing and saving screenshots of web elements.
+ */
+public class ScreenshotUtils {
+    /**
+     * Captures a screenshot of a web element and saves it to a file.
+     *
+     * @param element        The web element to capture a screenshot of.
+     * @param screenshotName The name to use for the saved screenshot file.
+     */
+	public void captureScreenshotfromPage(WebElement element, String screenshotName) {
+		try {
+			File source = element.getScreenshotAs(OutputType.FILE);
+			File destination = new File("C:\\eclipse-workspace\\resolver-assessment\\test-output\\" + screenshotName + ".png");
+			FileUtils.copyFile(source, destination);
+			System.out.println("Screenshot saved to Path " + screenshotName + ".png");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
